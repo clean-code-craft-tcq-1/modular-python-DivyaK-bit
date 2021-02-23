c22cc7cc53f92ad9c2b86233e95da29b02639160
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-02-23 16:33:52 +0530

    Update ReferenceFile.py

--- ReferenceFile.py
+++ ReferenceFile.py
@@ -7,6 +7,6 @@ def createReferenceInfo():
     for pair_number in range(pair_number_start_index, pair_number_end_index):
         major_color, minor_color = pd.get_color_from_pair_number(pair_number)
         colorCode = pd.color_pair_to_string(major_color, minor_color)
-        reference_data.append({'Number': pair_number, 'Color Code pairs': ColorCode})
+        reference_data.append({'Number': pair_number, 'Color Code pairs': colorCode})
     return reference_data
 
