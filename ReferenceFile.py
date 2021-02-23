e27b604922d3826f67d1a1b1dd87b7293f659f2d
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-02-23 16:30:50 +0530

    Update ReferenceFile.py

--- ReferenceFile.py
+++ ReferenceFile.py
@@ -6,15 +6,7 @@ def createReferenceInfo():
     reference_data = []
     for pair_number in range(pair_number_start_index, pair_number_end_index):
         major_color, minor_color = pd.get_color_from_pair_number(pair_number)
-        formatted_colorpair = pd.color_pair_to_string(major_color, minor_color)
-        reference_data.append({'Pair Number': pair_number, 'Color Code pairs': formatted_colorpair})
+        colorPair = pd.color_pair_to_string(major_color, minor_color)
+        reference_data.append({'Pair Number': pair_number, 'Color Code pairs': colorPair})
     return reference_data
 
-def PairList(refInfo, colList=None):
-    if not colList: colList = list(refInfo[0].keys() if refInfo else [])
-    myList = [colList]
-    for item in refInfo: myList.append([str(item[col] if item[col] is not None else '') for col in colList])
-    colSize = [max(map(len,col)) for col in zip(*myList)]
-    formatStr = ' | '.join(["{{:<{}}}".format(i) for i in colSize])
-    myList.insert(1, ['-' * i for i in colSize])
-    for item in myList: print(formatStr.format(*item))
