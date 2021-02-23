d2c05c9fdff330a1ff9b36c2befc84c61f8ad3d1
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-02-23 15:58:58 +0530

    Update ReferenceFile.py

--- ReferenceFile.py
+++ ReferenceFile.py
@@ -12,3 +12,11 @@ def createReferenceInfo():
         reference_data.append({'Pair Number': pair_number, 'Color Code pairs': formatted_colorpair})
     return reference_data
 
+def printTable(refData, colList=None):
+    if not colList: colList = list(refData[0].keys() if refData else [])
+    myList = [colList] # 1st row = header
+    for item in refData: myList.append([str(item[col] if item[col] is not None else '') for col in colList])
+    colSize = [max(map(len,col)) for col in zip(*myList)]
+    formatStr = ' | '.join(["{{:<{}}}".format(i) for i in colSize])
+    myList.insert(1, ['-' * i for i in colSize]) # Seperating line
+    for item in myList: print(formatStr.format(*item))
