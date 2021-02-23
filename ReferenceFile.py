22236703242a5c1bd0beb26de5103544a30ebea1
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-02-23 16:17:39 +0530

    Update ReferenceFile.py

--- ReferenceFile.py
+++ ReferenceFile.py
@@ -1,12 +1,12 @@
-import PairDefination as cpg
+import PairDefination as pd
 
 def createReferenceInfo():
     pair_number_start_index = 1
     pair_number_end_index = 26     
     reference_data = []
     for pair_number in range(pair_number_start_index, pair_number_end_index):
-        major_color, minor_color = cpg.get_color_from_pair_number(pair_number)
-        formatted_colorpair = cpg.color_pair_to_string(major_color, minor_color)
+        major_color, minor_color = pd.get_color_from_pair_number(pair_number)
+        formatted_colorpair = pd.color_pair_to_string(major_color, minor_color)
         reference_data.append({'Pair Number': pair_number, 'Color Code pairs': formatted_colorpair})
     return reference_data
 
