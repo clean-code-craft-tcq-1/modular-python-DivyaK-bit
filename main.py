7e62617dc919c6c1fa4710a8e72b7452b7d97002
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-02-23 16:03:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
   test_pair_to_number('Black', 'Orange', 12)
   test_pair_to_number('Violet', 'Slate', 25)
   test_pair_to_number('Red', 'Orange', 7)
-  printTable(createReferenceData())
+  printTable(createReferenceInfo())
   print('Done :)')
