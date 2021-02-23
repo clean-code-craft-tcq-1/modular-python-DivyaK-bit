abed1ab6fffb23adb1c4e043007fe7f6209a400b
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-02-23 15:55:39 +0530

    Update ReferenceFile.py

--- ReferenceFile.py
+++ ReferenceFile.py
@@ -1,1 +1,14 @@
+from ColorPairNames import *
+from ColorPairNames import *
+import ColorPairGenerator as cpg
+
+def createReferenceData():
+    pair_number_start_index = 1
+    pair_number_end_index = 26     
+    reference_data = []
+    for pair_number in range(pair_number_start_index, pair_number_end_index):
+        major_color, minor_color = cpg.get_color_from_pair_number(pair_number)
+        formatted_colorpair = cpg.color_pair_to_string(major_color, minor_color)
+        reference_data.append({'Pair Number': pair_number, 'Color Code pairs': formatted_colorpair})
+    return reference_data
 
