f49af222cf1646e2daf33a5ec428244bfa45322d
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-02-23 16:47:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from ReferenceFile import *
 from test_colorPair import *
 
+def PrintData(refdata)
+  for code in refdata
+  print(code)
+  
+  
 if __name__ == '__main__':
   test_number_to_pair(4, 'White', 'Brown')
   test_number_to_pair(5, 'White', 'Slate')
   test_pair_to_number('Black', 'Orange', 12)
   test_pair_to_number('Violet', 'Slate', 25)
   test_pair_to_number('Red', 'Orange', 7)
-  createReferenceInfo()
+  PrintData(createReferenceInfo())
   print('Done :)')
